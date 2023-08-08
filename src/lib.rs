19eff1f412352faa825ca844c5f2047285b3f1bf
SW van Heerden <swvheerden@gmail.com>
2023-08-08 10:50:48 +0200

    feat: initial code (#1)
    This moves out the WASM build of TariCrypto to its own separate repo.
    This clean's up the main TariCrypto library and the WASM build to be
    more clean and without feature compiler flags.

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+// Copyright 2022 The Tari Project
+// SPDX-License-Identifier: BSD-3-Clause
+
+
+#[macro_use]
+extern crate std;
+
+
+mod keyring;
+
+pub mod commitments;
+pub mod key_utils;
+pub mod range_proofs;
+pub use keyring::KeyRing;
+
